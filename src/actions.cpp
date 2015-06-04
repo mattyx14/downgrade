0d9d56fe550529c537ab06982715f155a0677efd
Gabriel Linhares <skazw@live.com>
2015-06-04 16:43:23 -0300

    Fixes

--- src/actions.cpp
+++ src/actions.cpp
@@ -357,11 +357,6 @@ ReturnValue Actions::internalUseItem(Player* player, const Position& pos, uint8_
 
 bool Actions::useItem(Player* player, const Position& pos, uint8_t index, Item* item, bool isHotkey)
 {
-	if (!player->canDoAction()) {
-		return false;
-	}
-
-	player->setNextActionTask(nullptr);
 	player->setNextAction(OTSYS_TIME() + g_config.getNumber(ConfigManager::ACTIONS_DELAY_INTERVAL));
 	player->stopWalk();
 
@@ -374,18 +369,12 @@ bool Actions::useItem(Player* player, const Position& pos, uint8_t index, Item* 
 		player->sendCancelMessage(ret);
 		return false;
 	}
-
 	return true;
 }
 
 bool Actions::useItemEx(Player* player, const Position& fromPos, const Position& toPos,
                         uint8_t toStackPos, Item* item, bool isHotkey, Creature* creature/* = nullptr*/)
 {
-	if (!player->canDoAction()) {
-		return false;
-	}
-
-	player->setNextActionTask(nullptr);
 	player->setNextAction(OTSYS_TIME() + g_config.getNumber(ConfigManager::EX_ACTIONS_DELAY_INTERVAL));
 	player->stopWalk();
 
